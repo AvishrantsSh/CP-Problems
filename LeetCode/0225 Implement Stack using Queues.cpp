7f72ad4f517c31214b2d723af8e37b5b710ae907
Avishrant Sharma <avishrants@gmail.com>
2022-05-08 00:54:03 +0530

    Add files via upload

---
+++ LeetCode/0225 Implement Stack using Queues.cpp
@@ -0,0 +1,28 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {}
+    
+    void push(int x) {
+        q.push(x);
+        for (int i = 1; i < q.size(); i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
