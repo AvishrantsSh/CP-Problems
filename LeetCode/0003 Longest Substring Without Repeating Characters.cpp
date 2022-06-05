4cc13d4f81f1c9ba2224fdf266a27ad1080a98b2
Avishrant Sharma <avishrants@gmail.com>
2022-06-05 06:11:24 +0000

    05/06/2022

--- LeetCode/0003 Longest Substring Without Repeating Characters.cpp
+++ LeetCode/0003 Longest Substring Without Repeating Characters.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int arr[150] = {};
-        int maxlen = 0, pointer = 0, tmplen = 0;
+        int maxlen = 0, pointer = 0;
         for (int i = 0; i < s.length(); i++) {
             int x = s[i];
             arr[x] += 1;
-            tmplen += 1;
             
             while(arr[x] > 1){
                 arr[s[pointer]] -= 1;
-                tmplen -= 1;
                 pointer += 1;
             }
 
-            maxlen = maxlen > tmplen ? maxlen : tmplen;
+            maxlen = max(i - pointer + 1, maxlen);
         }
         return maxlen;
     }
