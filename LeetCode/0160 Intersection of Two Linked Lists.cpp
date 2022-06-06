19681688689fb2929a688f0ed127386cfb08823d
Avishrant Sharma <avishrants@gmail.com>
2022-06-06 04:47:24 +0000

    06/06/2022

---
+++ LeetCode/0160 Intersection of Two Linked Lists.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *ptr1 = headA, *ptr2 = headB;
+        while (ptr1 != ptr2) {
+            ptr1 = !ptr1 ? headB : ptr1->next;
+            ptr2 = !ptr2 ? headA : ptr2->next;
+        }
+        return ptr1;
+    }
+};
