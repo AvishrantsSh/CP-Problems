0b20c6449c126b29c5cb029e68e908ade593aa52
Avishrant Sharma <avishrants@gmail.com>
2022-06-09 07:54:17 +0000

    09/06/2022

---
+++ LeetCode/0077 Combinations.cpp
@@ -0,0 +1,40 @@
+// Non-backtracking Solution
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+		int i = 0;
+		vector<int> p(k, 0);
+		while (i >= 0) {
+			p[i]++;
+			if (p[i] > n) i--;
+			else if (i == k - 1) result.push_back(p);
+			else p[++i] = p[i];
+		}
+		return result;
+    }
+};
+
+// Backtracking Solution
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> p;
+    vector<vector<int>> combine(int n, int k) {
+        solve(1, n, k);
+        return ans;
+    }
+    
+    void solve(int index, int n, int k){
+        if(p.size() == k){
+            ans.push_back(p);
+            return;
+        }
+        
+        for(; index <= n; index++){
+            p.push_back(index);
+            solve(index + 1, n , k);
+            p.pop_back();
+        }
+    }
+};
